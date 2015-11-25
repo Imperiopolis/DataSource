c5e4a3bf41024bf605e6eacc9dcd09d6e2bc8799
Imperiopolis <me@trappdesign.net>
2015-11-25 13:45:43 -0800

    Fix extendable item init

--- DataSource/Item.swift
+++ DataSource/Item.swift
@@ -38,6 +38,8 @@ public class ExtendableItem: Item {
     public var subtitle: String?
     public var image: UIImage?
     public var cellType: Int = 0
+
+    public required init() {}
 }
 
 public struct DataItem: Item, StringLiteralConvertible {
